f3bb99e7da5e594f24fb3a95ca330333c7eaa30b
Frank Goodman <frank_goodman@brown.edu>
2014-09-25 23:54:04 -0400

    progress on determining simple ingredient names

--- scripts/ingredient_names0.py
+++ scripts/ingredient_names0.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import defaultdict
 from lxml import etree
 from glob import glob
 
@@ -24,14 +24,57 @@ def read_from(tokens):
 def parse_sexpr(s):
     return read_from(tokenize(s))
 
+def parse_recipe(r):
+    units = [
+        "cups", "cup",
+        "teaspoons", "teaspoon",
+        "cans", "can",
+        "tablespoons", "tablespoon",
+        "packages", "package",
+        "pints", "pint",
+        "pinches", "pinch",
+        "pounds", "pound",
+        "bunches", "bunch",
+        "bars", "bar",
+        "ounces", "ounce",
+        "quarts", "quart",
+        "dashes", "dash",
+        "containers", "container",
+        "slices", "slice",
+        "envelopes", "envelope",
+        "scoops", "scoop",
+        "jars", "jar",
+        "loaves", "loaf",
+        "cubes", "cube",
+
+    ]
+
+    r = reduce(lambda r, u: r.split(" %s " % u, 1)[-1], units, r)
+    r = r.split(",", 1)[0]
+    r = r.split(") ", 1)[-1]
+
+    return r.strip()
+
+bigrams_cnt = defaultdict(lambda: 0)
 def parse_parse(p):
-    # p = p[1][1:]
-    # comma = next((i for i, v in enumerate(p) if v[0] == ","), -1)
+    global bigrams_cnt
 
-    # if comma >= 0:
-    #     p = p[:comma]
+    unigrams = []
 
-    # p = filter(lambda sp: True, p)
+    stack = p
+    while stack:
+        i = p.pop(0)
+
+        if len(i) > 1 and isinstance(i[1], list):
+            stack += i[1:]
+        else:
+            unigrams.append(i)
+    unigrams = [[None, None]] + unigrams[1:] + [[None, None]]
+
+    
+
+    for a, b in zip(unigrams, unigrams[1:]):
+        bigrams_cnt[(a[0], b[1])] += 1
 
     return p
 
@@ -47,6 +90,13 @@ for recipe_file, parse_file in zip(glob("../annotated_recipes/*.xml"), glob("../
             
             for t, p in zip(recipe_lines, parse_lines):
                 print t
+                print parse_sexpr(p)
                 print parse_parse(parse_sexpr(p))
+                print parse_recipe(t)
                 print ""
-            
+
+sorted_bigrams = [p for p in sorted(bigrams_cnt, key=bigrams_cnt.get) if p[0] == "CD"]
+
+total = sum(bigrams_cnt.values())
+
+print [(b, float(bigrams_cnt[b]) / total) for b in sorted_bigrams]
