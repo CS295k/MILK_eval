11d54ae9968987440c3698e47b1ec39c076c2cb9
Frank Goodman <frank_goodman@brown.edu>
2014-09-26 14:59:46 -0400

    naive implementation of sentence head grabbing for simple ingredients

--- scripts/ingredient_names0.py
+++ scripts/ingredient_names0.py
@@ -55,28 +55,52 @@ def parse_recipe(r):
 
     return r.strip()
 
-bigrams_cnt = defaultdict(lambda: 0)
+# bigrams_cnt = defaultdict(lambda: 0)
 def parse_parse(p):
-    global bigrams_cnt
+    # global bigrams_cnt
 
-    unigrams = []
+    # unigrams = []
 
-    stack = p
-    while stack:
-        i = p.pop(0)
+    # stack = p
+    # while stack:
+    #     i = p.pop(0)
 
-        if len(i) > 1 and isinstance(i[1], list):
-            stack += i[1:]
-        else:
-            unigrams.append(i)
-    unigrams = [[None, None]] + unigrams[1:] + [[None, None]]
+    #     if len(i) > 1 and isinstance(i[1], list):
+    #         stack += i[1:]
+    #     else:
+    #         unigrams.append(i)
+    # unigrams = [[None, None]] + unigrams[1:] + [[None, None]]
 
     
 
-    for a, b in zip(unigrams, unigrams[1:]):
-        bigrams_cnt[(a[0], b[1])] += 1
+    # for a, b in zip(unigrams, unigrams[1:]):
+    #     bigrams_cnt[(a[0], b[1])] += 1
 
-    return p
+
+    # Get left-most NP
+    leftest = p
+    while True:
+        if len(leftest) > 1 and isinstance(leftest[1], list):
+            if leftest[0] == "NP":
+                break
+            leftest = leftest[1]
+        else:
+            leftest = None
+            break
+
+    # Get right-most NN, NNS, NNP, or NNPS
+    rightest = leftest
+    while True and (rightest is not None):
+        if len(rightest) > 1 and isinstance(rightest[-1], list):
+            if rightest[-1][0] in {"NN", "NNS", "NNP", "NNPS"}:
+                rightest = rightest[-1]
+                break
+            rightest = rightest[-1]
+        else:
+            rightest = None
+            break
+
+    return rightest
 
 for recipe_file, parse_file in zip(glob("../annotated_recipes/*.xml"), glob("../data/parsed_recipes/*.txt")):
     with open(recipe_file) as rf:
@@ -89,14 +113,25 @@ for recipe_file, parse_file in zip(glob("../annotated_recipes/*.xml"), glob("../
             parse_lines = pf.read().splitlines()[4::102]
             
             for t, p in zip(recipe_lines, parse_lines):
-                print t
-                print parse_sexpr(p)
-                print parse_parse(parse_sexpr(p))
-                print parse_recipe(t)
-                print ""
+                #print t
+                #print parse_parse(parse_sexpr(p))
+                #print ""
+                pp = parse_parse(parse_sexpr(p))
+                if pp is None:
+                    pp = "None"
+                else:
+                    pp = pp[1]
+                print "%s -> %s" % (t, pp)
 
-sorted_bigrams = [p for p in sorted(bigrams_cnt, key=bigrams_cnt.get) if p[0] == "CD"]
+# cd_bigrams_cnt = {k: v for k, v in bigrams_cnt.items() if k[0] == "CD"}
 
-total = sum(bigrams_cnt.values())
+# word_cnt = defaultdict(lambda: 0)
 
-print [(b, float(bigrams_cnt[b]) / total) for b in sorted_bigrams]
+# for k, v in bigrams_cnt.items():
+#     word_cnt[k[1]] += v
+
+# sorted_bigrams = [p for p in sorted(bigrams_cnt, key=bigrams_cnt.get) if p[0] == "CD"]
+
+# total = sum(bigrams_cnt.values())
+
+# print [(b, float(bigrams_cnt[b]) / word_cnt[b[1]]) for b in sorted_bigrams]
