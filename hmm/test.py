41b995fcede7699dc466e0f23cc62fe9dc69141f
Qi Xin <xinq07@gmail.com>
2014-10-06 11:49:42 -0400

    Fix test.py

--- hmm/test.py
+++ hmm/test.py
@@ -4,6 +4,8 @@ from probs_new import remove_create_ing
 from probs_new import get_sigmas
 from probs_new import get_taus
 from decoder import group_tagging
+from EM import forward_algorithm
+from EM import backward_algorithm
 
 if __name__ == "__main__":
 
@@ -23,22 +25,21 @@ if __name__ == "__main__":
     for (s, c), tau in taus.iteritems():
         taus_for_decoding[(s-1, c)] = tau
 
-    '''
-    cmds = ["combine", "create_tool", "combine", "cook", "combine", \
-            "combine", "combine", "create_tool", "create_tool", "cut", \
-            "put", "create_tool", "cook"]
-    '''
-
-
-
     # Decode
-    test_cmds = [["_".join(anns) for (ot, anns) in r] for r in test_recipes]
     n = 4
-    alltags = []
-
-    for cmd in test_cmds:
-        tags = group_tagging(n, cmd, sigmas_for_decoding, taus_for_decoding)
+    tagss0 = [ [len(anns) for (ot, anns) in r for i in xrange(len(anns))] for r in test_recipes]
+    tagss1 = []
+    test_cmdss = [[a for (ot, anns) in r for a in anns] for r in test_recipes]
+    for cmds in test_cmdss:
+        tags = group_tagging(n, cmds, sigmas_for_decoding, taus_for_decoding)
         tags = [tag+1 for tag in tags]
-        alltags.append(tags)
+        tagss1.append(tags)
 
-    print alltags
+    print "True tags"
+    print tagss0
+    print "Generated tags"
+    print tagss1
+
+
+
+
