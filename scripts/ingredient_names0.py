bc0d428dcfcacfafa94d92c975cd995fbafb20f3
Frank Goodman <frank_goodman@brown.edu>
2014-10-11 11:05:03 -0400

    preliminary work on new and improved head finding script

--- scripts/ingredient_names0.py
+++ scripts/ingredient_names0.py
@@ -3,94 +3,78 @@ from lxml import etree
 from glob import glob
 
 
-class Sexpr(object):
-
-    def __init__(self, sexpr):
-        self.sexpr = sexpr
-
-    def tokenize(self, sexpr):
-        return sexpr.replace("(", " ( ").replace(")", " ) ").split()
-
-    def read_from(self, tokens):
-        if not tokens:
-            raise SyntaxError("Sexpr: Unexpect EOF while reading.")
-
-        token = tokens.pop(0)
-        if token == "(":
-            L = []
-            while tokens[0] != ")":
-                L.append(self.read_from(tokens))
-            tokens.pop(0)
-            return L
-        elif token == ")":
-            raise SyntaxError("Sexpr: Unexpect ')' while reading.")
-        return str(token)
-
-    def to_list(self):
-        return self.read_from(self.tokenize(self.sexpr))
+def create_annotated_recipe_generator(n = None):
+    i = 0
+    for annotated_recipe in glob("../annotated_recipes/*.xml"):
+        with open(recipe) as r:
+            elt = etree.XML(r.read())
+            originaltexts = [line.text for line in elt.findall(".//originaltext")]
+            annotations = [line.text for line in elt.findall(".//annotation")]
+            yield (originaltexts, annotations)
+            i += 1
+            if n and i >= n: break
 
 
-def create_recipe_generator(recipe_file = "../annotated_recipes/*.xml", parse_file = "../data/parsed_recipes/*.txt"):
+def create_parsed_recipe_generator(n = None):
+    def parse(sexpr):
+        tokens = sexpr.replace("(", " ( ").replace(")", " ) ").split()
+
+        def read_from(tokens):
+            if not tokens: raise SyntaxError("Sexpr: Unexpected EOF while reading.")
+
+            token = tokens.pop(0)
+            if token == "(":
+                L = []
+                while tokens[0] != ")":
+                    L.append(read_from(tokens))
+                tokens.pop(0)
+                return L
+            elif token == ")":
+                raise SyntaxError("Sexpr: Unexpected ')' while reading.")
+
+            return str(token)
+
+        return read_from(tokens)
+
+
     def chunks(l, n):
         for i in xrange(0, len(l), n):
             yield l[i:i + n]
 
-    for recipe, parse in zip(glob(recipe_file), glob(parse_file)):
-        with open(recipe) as r:
-            elt = etree.XML(r.read())
-            originaltext = [line.text for line in elt.findall(".//originaltext")]
-            annotation_cmd = [line.text.split("(", 1)[0] for line in elt.findall(".//annotation")]
-            last_create_ing = next((i for i, v in enumerate(annotation_cmd) if v != "create_ing"), -1)
-            create_ing = originaltext[:last_create_ing]
-            with open(parse) as p:
 
-                originaltext_parses = list(chunks(p.read().splitlines(), 102))
-
-                for originaltext, originaltext_parses in zip(create_ing, originaltext_parses):
-                    yield (originaltext, [Sexpr(parse).to_list() for parse in originaltext_parses[2:-1:2]])
+    i = 0
+    for parsed_recipe in glob("../data/parsed_recipes/*.txt"):
+        with open(parsed_recipe) as r:
+            originaltext_parses = list(chunks(r.read().splitlines(), 102))
+            yield [[parse(p) for p in op[2:-1:2]] for op in originaltext_parses]
+            i += 1
+            if n and i >= n: break
 
 
-class Ingredient(object):
-
-    def __init__(self, sexprs):
-        self.sexprs = sexprs
-
-    def simplify(self):
-
-        # sexpr: top ranked parse with a top-level NP
-        for sexpr in self.sexprs:
-            if "NP" in [s[0] for s in sexpr[1:]]: break
+def find_head(parses):
+    for p in parses:
+        if "NP" in [p_[0] for p_ in p[1:]]:
+            break
         else:
             return None
 
-        # NPs: all NPs in parse tree found by pre-order DFS
-        NPs = [sexpr]
-        stack = sexpr[1:]
-        while stack:
-            node = stack.pop(0)
-            if node[0] == "NP":
-                NPs.append(node)
-            
+    NPs = [p]
+    stack = p[1:]
+    while stack:
+        node = stack.pop(0)
+        if node[0] == "NP":
+            NPs.append(node)
+
             if isinstance(node, list):
                 stack = node[1:] + stack
 
-        # r: all NN, NNS, NNP, NNPS found
-        for NP in NPs[::-1]:
-            r = [p[1] for p in NP[1:] if p[0] in {"NN", "NNS", "NNP", "NNPS"}]
-            if r:
-                return r[-1]
+    for NP in NPs[::-1]:
+        r = [p[1] for p in NP[1:] if p[0] in {"NN", "NNS", "NNP", "NNPS"}]
+        if r:
+            return r[-1]
         else:
             return None
 
 
 if __name__ == "__main__":
-    for originaltext, originaltext_parses in create_recipe_generator():
-<<<<<<< HEAD
-        try:
-            print "%s -> %s" % (originaltext, Ingredient(originaltext_parses).simplify())
-        except UnicodeEncodeError:
-            pass
-
-=======
-        print "%s -> %s" % (originaltext, Ingredient(originaltext_parses).simplify())
->>>>>>> 1d13efcda69c1ddaf5a1b1b1491783957b3ef289
+    # WIP
