ad9bbc2f1d805219c0802eae37c00a1c7a85d0e0
Ben Weedon <turambar@comcast.net>
2014-10-21 23:46:56 -0400

    Updated Data_Interface

--- Data_Interface.py
+++ Data_Interface.py
@@ -1,8 +1,16 @@
+from glob import glob
 from xml.dom.minidom import parse
+from os.path import basename
 
-def getParseCommandPairs(file):
-	pairs = []
-	dom = parse(file)
-	for elt in dom.getElementsByTagName("line")
-		pairs.append((elt.getElementsByTagName("parsed-text")[0].childNodes[0].data, elt.getElementsByTagName("annotation")[0].childNodes[0].data))
-	return pairs
+def getParseCommandPairMappings():
+	'''
+	Returns a dictionary mapping from filename to list of parse-command pairs
+	'''
+	fileMappings = {}
+	for filename in glob("parsed_annotated_recipes/*.xml"):
+		pairs = []
+		dom = parse(filename)
+		for elt in dom.getElementsByTagName("line"):
+			pairs.append((elt.getElementsByTagName("parsed-text")[0].childNodes[0].data, elt.getElementsByTagName("annotation")[0].childNodes[0].data))
+		fileMappings[basename(filename)[:-4]] = pairs
+	return fileMappings
