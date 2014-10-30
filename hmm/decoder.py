a11c12ad0f8d69825f46b366a12911f13bd8f78f
xinq07 <xinq07@gmail.com>
2014-10-30 15:28:00 -0400

    Best-k HMM Update

--- hmm/decoder.py
+++ hmm/decoder.py
@@ -16,14 +16,39 @@ def getCmdStr(cmds, i, j):
     
 # End of Func
 
-def group_tagging(n, cmds, sigmas, taus):
+def getSigma(state1, state2, sigmas):
+
+    # Smooth
+    if (state1, state2) not in sigmas:
+        sigma = 1e-5
+    else:
+        sigma = sigmas[(state1,state2)]
+        if (sigma == 0):
+            sigma = 1e-5
+    return sigma
+            
+def getTau(i, j, cmdStr, taus):
+
+    # Smooth
+    if cmdStr is None:
+        tau = 0
+    elif (j, cmdStr) not in taus:
+        tau = 1e-5
+    else:
+        tau = taus[(j, cmdStr)]
+        if (tau == 0):
+            tau = 1e-5
+    return tau
+
+def group_tagging(n, cmds, sigmas, taus, best_num):
 
     # INPUT:
     # n: the number of states
     # cmds: a list of strings of command
     # sigmas: a dict with key: (state1, state2) & val: prob
     # taus: a dict with key:(state, commandStr) & val: prob
-    # 
+    # best_num: find best sequences in number of best_num
+    #
     # NOTE:
     # 1. State begins with 0
     # 2. commandStr is like
@@ -31,114 +56,115 @@ def group_tagging(n, cmds, sigmas, taus):
     # "combine_put_do" for multiple commands
 
     # RETURN:
-    # tags: a list of grouping labels
+    # tagss: best_num lists of grouping labels
 
     len_states = n
     len_cmds = len(cmds)
 
     # Matrix Initialization
-    # mu: log(len_states x len_cmds)
-    mus = [[ 0 for j in xrange(len_states) ] for i in xrange(len_cmds)]
-    labels = [[ (-1,-1) for j in xrange(len_states)] for i in xrange(len_cmds)]
+    # size of log_mus & labels: len_states x len_cmds x k
+    log_mus = [[ [float('-Inf') for k in xrange(best_num)] for j in xrange(len_states) ] for i in xrange(len_cmds)]
+    labels = [[ [(-2,-2,-2) for k in xrange(best_num)] for j in xrange(len_states)] for i in xrange(len_cmds)]
 
-    # Build up mus & labels
+    # Build up log_mus & labels
     for i in xrange(len_cmds):
         for j in xrange(len_states):
             # Get tau
             cmdStr = getCmdStr(cmds, i, j)
-            if cmdStr is None:
-                #tau_j_i = 0.0
-                log_tau_j_i = float('-Inf')
+            tau = getTau(i, j, cmdStr, taus)
+            if (tau == 0):
+                log_tau = float('-Inf')
             else:
-                # Assume (j, cmdStr) contains
-                if (j, cmdStr) not in taus:
-                    #tau_j_i = 1e-5
-                    log_tau_j_i = math.log(1e-5)
-                else:
-                    tau_j_i = taus[(j, cmdStr)]
-                    if (tau_j_i == 0):
-                        log_tau_j_i = math.log(1e-5)
-                    else:
-                        log_tau_j_i = math.log(tau_j_i)
+                log_tau = math.log(tau)
 
             if (i == 0):
-                #mus[i][j] = 1.0 * tau_j_i
-                mus[i][j] = log_tau_j_i
-                # The start symbol
-                labels[i][j] = (-1,-1)
+                #mus[0][j][0] = 1.0 * tau
+                log_mus[i][j][0] = log_tau
+                #label[0][j][0] is the start symbol
+                labels[i][j][0] = (-1,-1,-1)
                 
             else:
-                # possible moves from the most recent
-                #best_mu = 0
-                best_mu = float('-Inf')
-                best_last = (-1,-1)
-                for k in xrange(len_states):
-                    last_i = i-k-1
+                last_best_num_index_list = [ 0 for k in xrange(len_states) ]
+                for best_iter in xrange(best_num):
+                    # Possible moves from the last state k,
+                    # Find the largest to fill mus/labels[i][j][best_iter]
+                    best_log_mu = float('-Inf')
+                    best_label = (-2,-2,-2)
+                    for k in xrange(len_states):
+                        last_i = i-k-1
+                        if (last_i >= 0):
+                            # Valid move from state k to j
+                            last_best_num_index = last_best_num_index_list[k]
+                            last_log_mu = log_mus[last_i][k][last_best_num_index]
+                            sigma = getSigma(k, j, sigmas)
+                            log_sigma = math.log(sigma)
+                            current_log_mu = last_log_mu + log_sigma + log_tau
+                            if (current_log_mu > best_log_mu):
+                                best_log_mu = current_log_mu
+                                best_label = (last_i, k, last_best_num_index)
+                             
+                    # End of forloop k
+                    best_k = best_label[1]
+                    if (best_k != -2):
+                        last_best_num_index_list[best_k] += 1
+                    log_mus[i][j][best_iter] = best_log_mu
+                    labels[i][j][best_iter] = best_label
 
-                    # Get last mu & the sigma
-                    mu_lasti_k = float('-Inf')
-                    sigma_k_j = 0
-                    if (last_i < 0):
-                        # Can't move by k+1
-                        pass
-                    else:
-                        # States from k to j
-                        mu_lasti_k = mus[last_i][k]
-                        #sigma_k_j = sigmas[(k, j)]
-                        if (k, j) not in sigmas:
-                            log_sigma_k_j = math.log(1e-5)
-                        else:
-                            sigma_k_j = sigmas[(k,j)]
-                            if (sigma_k_j == 0):
-                                log_sigma_k_j = math.log(1e-5)
-                            else:
-                                log_sigma_k_j = math.log(sigma_k_j)
-                    # End of else
+    # Build up end_log_mus & end_labels
+    # end_log_mus[0:best_num] <=> log_mus[len_cmds+1][<|][0:best_num]
+    # end_labels[0:best_num] <=> labels[len_cmds+1][<|][0:best_num]
 
-                    #current_mu = mu_lasti_k * sigma_k_j * tau_j_i
-                    current_mu = mu_lasti_k + log_sigma_k_j + log_tau_j_i
-                    if (current_mu > best_mu):
-                        best_mu = current_mu
-                        best_last = (last_i, k)
-                        
-                # End of forloop k
-                mus[i][j] = best_mu
-                labels[i][j] = best_last
-                    
-            # End of else
-                
-        # End of State loop
-        
-    # End of Cmd loop
+    #########################
+    #print "End up building log_mus & labels"
+    #########################
+    
+    end_log_mus = [ float('-Inf') for k in xrange(best_num) ]
+    end_labels = [ (-2,-2,-2) for k in xrange(best_num) ]
+    last_best_num_index_list = [ 0 for k in xrange(len_states) ]
+    for best_iter in xrange(best_num):
+        best_log_mu = float('-Inf')
+        best_label = (-2,-2,-2)
+        for k in xrange(len_states):
+            last_i = len_cmds-k-1
+            if (last_i >= 0):
+                # Valid move from state k to <|
+                last_best_num_index = last_best_num_index_list[k]
+                last_log_mu = log_mus[last_i][k][last_best_num_index]
+                current_log_mu = last_log_mu
+                if (current_log_mu > best_log_mu):
+                    best_log_mu = current_log_mu
+                    best_label = (last_i, k, last_best_num_index)
+        # End of forloop k
+        best_k = best_label[1]
+        if (best_k != -2):
+            last_best_num_index_list[best_k] += 1
+        end_log_mus[best_iter] = best_log_mu
+        end_labels[best_iter] = best_label
 
-    # Trace back
-    best_end_mu = float('-Inf')
-    best_end_k = -1
-    for k in xrange(len_states):
-        i = len_cmds - k - 1
-        if (i < 0):
-            continue
-        if (mus[i][k] > best_end_mu):
-            best_end_mu = mus[i][k]
-            best_end_k = k
-    # End of for loop
-    best_end_i = len_cmds - best_end_k - 1
 
-    # Create the tags
-    tags = []
-    last_i = best_end_i
-    last_j = best_end_k
-    for k in xrange(len_cmds-1, last_i-1, -1):
-        tags.insert(0, last_j)
-    while (last_i != 0):
-        (i, j) = labels[last_i][last_j]
-        for k in xrange(last_i-1, i-1, -1):
-            tags.insert(0, j)
-        last_i = i
-        last_j = j
-    # End of While loop
+    #########################
+    #print "End up building end_mus & end_labels"
+    #########################
 
-    return tags
+    # Create the tagss
+    # Note: the size of tagss could be LESS than best_num
+    tagss = []
+    for best_iter in xrange(best_num):
+        tags = []
+        if (end_log_mus[best_iter] == float('-Inf')):
+            break
+        (last_i, last_j, last_index) = end_labels[best_iter]
+        # From len_cmds-1 back to last_i, label as last_j
+        for k in xrange(len_cmds-1, last_i-1, -1):
+            tags.insert(0, last_j)
+        while (last_i != 0):
+            (i, j, index) = labels[last_i][last_j][last_index]
+            for k in xrange(last_i-1, i-1, -1):
+                tags.insert(0, j)
+            last_i, last_j, last_index = i, j, index
+        tagss.append(tags)
+
+    return tagss
     
 # End of func
 
@@ -151,8 +177,9 @@ if __name__ == "__main__":
             (1, "combine_put"): 0.4, (1, "combine_do"): 0.2, (1, "put_do"): 0.1, \
             (1, "put_combine"): 0.1, (1, "do_combine"): 0.1, (1, "do_put"): 0.1}
     n = 2
+    best_num = 3
 
-    tags = group_tagging(n, cmds, sigmas, taus)
-    print(tags)
+    tagss = group_tagging(n, cmds, sigmas, taus, best_num)
+    print(tagss)
     
 
--- hmm/test.py
+++ hmm/test.py
@@ -12,8 +12,8 @@ from glob import glob
 from sklearn.cross_validation import train_test_split
 
 if __name__ == "__main__":
-  # train_path = "./train/*.xml"
-  # test_path = "./test/*.xml"
+  #train_paths = glob("./train/*.xml")
+  #test_paths = glob("./test/*.xml")
 
   data_files = glob("../annotated_recipes/*.xml")
   train_paths, test_paths = train_test_split(data_files, test_size=0.25)
@@ -40,32 +40,40 @@ if __name__ == "__main__":
 
   # Decode
   n = 4
+  best_num = 100
   tagss0 = [[len(anns) for (ot, anns) in r for _ in anns] for r in test_recipes]
-  tagss1 = []
+  best_tagss1 = []
   test_cmdss = [[a for (ot, anns) in r for a in anns] for r in test_recipes]
 
-  for cmds in test_cmdss:
-    tags = group_tagging(n, cmds, sigmas_for_decoding, taus_for_decoding)
-    tags = [tag+1 for tag in tags]
-    tagss1.append(tags)
+  # Build up best_tagss1
+  for tags0, test_cmds in zip(tagss0, test_cmdss):
+    tags1_list = group_tagging(n, test_cmds, sigmas_for_decoding, taus_for_decoding, best_num)
+    # From tags1_list, find the best and add to best_tagss1
+    best_fscore = 0
+    best_tags1 = []
+    for tags1 in tags1_list:
+      tags1 = [tag+1 for tag in tags1]
+      fscore = getFScore([tags0], [tags1])
+      if (fscore > best_fscore):
+        best_fscore = fscore
+        best_tags1 = tags1
+    best_tagss1.append(best_tags1)
 
-  # print "True tags"
-  # print tagss0
-  # print "Generated tags"
-  # print tagss1
-
+  '''
   for fn, true, pred in zip(test_paths, tagss0, tagss1):
     print fn
     print "True tags", true
     print "Pred tags", pred
     print
 
-  print 'sigma:'
-  for i in range(n):
-    print [sigmas_for_decoding.get((i, j), 0) for j in range(n)]
-  print
 
-  fscore = getFScore(tagss0, tagss1)
+    print 'sigma:'
+    for i in range(n):
+      print [sigmas_for_decoding.get((i, j), 0) for j in range(n)]
+    print
+  '''
+
+  fscore = getFScore(tagss0, best_tagss1)
   print "F-Scores"
   print fscore
 
