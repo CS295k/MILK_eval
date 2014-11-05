7cca3d3c543b431a20a6dad4b1d061aef9c6e47e
xinq07 <xinq07@gmail.com>
2014-11-05 08:51:44 -0500

    Update MILKToEnglish.py, add HMM part

--- hmm/MILKToEnglish.py
+++ hmm/MILKToEnglish.py
@@ -8,4 +8,24 @@ import random
 import operator
 from glob import glob
 from JITDecoder import JITDecoder
+from group_tagger import group_tagger
+from sklearn.cross_validation import train_test_split
 
+if __name__ == "__main__":
+
+    data_files = glob("../annotated_recipes/*.xml")
+    train_paths, test_paths = train_test_split(data_files, test_size=0.25)
+    
+    tagger = group_tagger(train_paths, test_paths)
+    
+    ############
+    # To use
+    test_recipe_index = 0
+    group_num = 4
+    best_seq_num = 100
+    jit_decoder = tagger.get_JITDecoder(test_recipe_index, group_num, best_seq_num)
+    jit_decoder.ping();
+    jit_decoder.select(2);
+    jit_decoder.ping();
+    ############
+    
