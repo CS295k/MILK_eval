bd8e05032204b1037dca3bd1dea9049488ddd40e
David Whitney <david_whitney@brown.edu>
2014-12-11 21:22:25 -0500

    head fallback

--- scripts/NP_generator.py
+++ scripts/NP_generator.py
@@ -2,16 +2,27 @@ import operator
 import collections
 import random
 import click
+import ingredient_names2
 
 data_file = "train_test_NP_list.txt"
+head_file = "simple_ingredients3.txt"
 
 command_list = []
+head_list = {}
 
 def file_reader():
 	f = open(data_file, 'r')
 	for line in f:
 		command_list.append(line.rstrip('\n').encode('utf-8'))
 	f.close()
+
+def head_reader():
+	f = open(head_file, 'r')
+	for line in f:
+		split_line = line.decode('utf-8').rstrip('\n').split(' -> ')
+		#print line
+		head_list[split_line[0]] = split_line[1]
+
 
 def normalize(x):
 	total = sum(x.values(), 0.0)
@@ -44,8 +55,10 @@ def gen_NP(command, input_NP, mod, isFromCommandLine = False):
 					if input_NP in cur_input:
 						cur_output = cur_NPs[1]
 						outputs[cur_output] += 1
-	if(command != "combine"):
+	if(command != "combine" and not outputs):
 		outputs[input_NP] += 1
+	if(command == 'create_ing' and input_NP in head_list):
+		outputs[head_list[input_NP]] += 1
 	if not outputs:
 		outputs = length_outputs
 	normalize(outputs)
@@ -55,6 +68,7 @@ def gen_NP(command, input_NP, mod, isFromCommandLine = False):
 
 def main():
 	file_reader()
+	head_reader()
 	command = click.prompt('Enter command', type=str)
 	input_NP = click.prompt('Enter input NP', type=str)
 	mod = click.prompt('Enter mod (0-9)', type=click.IntRange(0, 9))
